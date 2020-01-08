69cebb28d5701edee802763a13563aa26e869eef
ARNOLD D SOUZA <arnold.dsouza@in.ibm.com>
2020-01-08 15:38:29 +0530

    Added console from get

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const express = require('express')
 
+console.log('OPENSHIFT_NODEJS_PORT:', process.env.OPENSHIFT_NODEJS_PORT)
+console.log('OPENSHIFT_NODEJS_IP:', process.env.OPENSHIFT_NODEJS_IP)
+
+console.log('PORT:', process.env.PORT)
+console.log('IP:', process.env.IP)
+
 const port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 const ipAddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
@@ -8,6 +14,11 @@ const app = express()
 app.get('/', (req, res) => {
   console.log('Received a HTTP request!')
   res.send('Hello world')
+})
+
+app.get('/home', (req, res) => {
+  console.log('Received a HTTP request!')
+  res.send('Welcome to the home page!')
 })
 
 app.listen(port, ipAddress, () => {
