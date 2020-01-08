28b7caee294e42275231bd0ef99baebbd787e0ab
ARNOLD D SOUZA <arnold.dsouza@in.ibm.com>
2020-01-08 13:39:36 +0530

    Added port

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 
+const port = 8080
+
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-app.listen(3000)
+app.listen(port)
 
-console.log('Server is running on port 3000')
+console.log('Server is running on port: ' + port)
