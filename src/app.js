dff9d4f6decfe06160258214680c12f743a31698
ARNOLD D SOUZA <arnold.dsouza@in.ibm.com>
2020-01-09 17:17:30 +0530

    Refactored to reduce unnecessary code

--- src/app.js
+++ src/app.js
@@ -1,22 +1,11 @@
 const express = require('express')
 
-console.log('RUNNING THE HELLO WORLD APPLICATION!')
-
-const port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-const ipAddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-
 const app = express()
 
 app.get('/', (req, res) => {
-  console.log('Received a HTTP request!')
-  res.send('Hello world')
+  res.send('<h1>Welcome to our Node.js application!</h1>')
 })
 
-app.get('/home', (req, res) => {
-  console.log('Received a HTTP request!')
-  res.send('Welcome to the home page!')
+app.listen(8080, () => {
+  console.log('Server is listening on the port: 8080')
 })
-
-app.listen(8080)
-
-console.log('Server is up on 8080')
